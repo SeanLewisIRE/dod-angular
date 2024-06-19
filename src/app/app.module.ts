b072bb9389b65784064bf8fd470db3bfb46db70a
Sean <slewis100@gmail.com>
2024-06-19 22:19:08 +0100

    Mobile in good shape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './shared/components/card/card.component';
 import { MatCardModule } from '@angular/material';
 import { ContactFormComponent } from './shared/components/contact-form/contact-form.component';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -39,7 +40,8 @@ const routes: Routes = [
     AppRoutingModule,
     CardComponent,
     MatCardModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    MatIconModule
   ],
   exports: [RouterModule],
   providers: [],
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  heroTopSpan: string = ""
+  heroImage: string = "../../../../assets/images/ice.jpg"
+  heroHeader: string = "Contact"
+  heroBottomSpan: string = ""
+  heroStyle: Array<string> = ['h-screen-25']
+  heroTextStyle: Array<string> = ['absolute', 'inset-0', 'flex', 'items-center', 'justify-center', 'mx-auto', 'text-shadow']
+  blanketCardStyle: Array<string> = ["rounded", "h-full", "mx-10"]
 }
