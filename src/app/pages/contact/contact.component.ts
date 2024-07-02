b3de525d71b3f521bd1c62b4aeea1d19380c474c
Sean <slewis100@gmail.com>
2024-07-02 22:28:00 +0100

    Most sizes working

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -11,7 +11,7 @@ export class ContactComponent {
   heroImage: string = "../../../../assets/images/ice.jpg"
   heroHeader: string = "Contact"
   heroBottomSpan: string = ""
-  heroStyle: Array<string> = ['h-screen-25']
+  heroStyle: Array<string> = ['h-screen-25', 'lg:h-screen-50']
   heroTextStyle: Array<string> = ['absolute', 'inset-0', 'flex', 'items-center', 'justify-center', 'mx-auto', 'text-shadow']
   blanketCardStyle: Array<string> = ["rounded", "h-full", "mx-10"]
 }
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -11,7 +11,7 @@ export class ServicesComponent {
   heroImage: string = "../../../../assets/images/ice.jpg"
   heroHeader: string = "Services"
   heroBottomSpan: string = ""
-  heroStyle: Array<string> = ['h-screen-25']
+  heroStyle: Array<string> = ['h-screen-25', 'lg:h-screen-50']
   heroTextStyle: Array<string> = ['absolute', 'inset-0', 'flex', 'items-center', 'justify-center', 'mx-auto', 'text-shadow']
   blanketCardStyle: Array<string> = ["rounded", "h-full", "mx-10", "lg:mx-5"]
 
