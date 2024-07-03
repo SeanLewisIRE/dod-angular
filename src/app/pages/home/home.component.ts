e362079fa22d44c0645b3cea4d142e24abf3003b
Sean <slewis100@gmail.com>
2024-07-03 11:49:29 +0100

    Single page experiment

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -63,4 +63,81 @@ export class HomeComponent {
     },
   ]
 
+  // heroTopSpan: string = ""
+  // heroImage: string = "../../../../assets/images/ice.jpg"
+  // heroHeader: string = "Services"
+  // heroBottomSpan: string = ""
+  // heroStyle: Array<string> = ['h-screen-25', 'lg:h-screen-50']
+  // heroTextStyle: Array<string> = ['absolute', 'inset-0', 'flex', 'items-center', 'justify-center', 'mx-auto', 'text-shadow']
+  blanketCardStyle: Array<string> = ["rounded", "h-full", "mx-10", "lg:mx-5"]
+
+  servicesCardData = [
+    {
+      headerSubtitle: "subtitle",
+      header: "header",
+      firstPara: "firstPara",
+      secondPara: "secondPara",
+      link: "./",
+      linkText: "LNK TEXT",
+      cardClass: ['white-service'].concat(this.blanketCardStyle),
+      cardImage: "https://picsum.photos/100",
+      cardImageStyle: ["rounded-full", "mx-auto", "h-20", "w-20"]
+    },
+    {
+      headerSubtitle: "subtitle",
+      header: "header",
+      firstPara: "firstPara",
+      secondPara: "secondPara",
+      link: "./",
+      linkText: "LNK TEXT",
+      cardClass: ['orange-service'].concat(this.blanketCardStyle),
+      cardImage: "https://picsum.photos/100",
+      cardImageStyle: ["rounded-full", "mx-auto", "h-20", "w-20"]
+    },
+    {
+      headerSubtitle: "subtitle",
+      header: "header",
+      firstPara: "firstPara",
+      secondPara: "secondPara",
+      link: "./",
+      linkText: "LNK TEXT",
+      cardClass: ['white-service'].concat(this.blanketCardStyle),
+      cardImage: "https://picsum.photos/100",
+      cardImageStyle: ["rounded-full", "mx-auto", "h-20", "w-20"]
+    },
+    {
+      headerSubtitle: "subtitle",
+      header: "header1",
+      firstPara: "firstPara",
+      secondPara: "secondPara",
+      link: "./",
+      linkText: "LNK TEXT",
+      cardClass: ['orange-service'].concat(this.blanketCardStyle),
+      cardImage: "https://picsum.photos/100",
+      cardImageStyle: ["rounded-full", "mx-auto", "h-20", "w-20"]
+    },
+    {
+      headerSubtitle: "subtitle",
+      header: "header",
+      firstPara: "firstPara",
+      secondPara: "secondPara",
+      link: "./",
+      linkText: "LNK TEXT",
+      cardClass: ['white-service'].concat(this.blanketCardStyle),
+      cardImage: "https://picsum.photos/100",
+      cardImageStyle: ["rounded-full", "mx-auto", "h-20", "w-20"]
+    },
+    {
+      headerSubtitle: "subtitle",
+      header: "header last",
+      firstPara: "firstPara",
+      secondPara: "secondPara",
+      link: "./",
+      linkText: "LNK TEXT",
+      cardClass: ['orange-service'].concat(this.blanketCardStyle),
+      cardImage: "https://picsum.photos/100",
+      cardImageStyle: ["rounded-full", "mx-auto", "h-20", "w-20"]
+    },
+  ]
+
 }
